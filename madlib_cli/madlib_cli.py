2b40859dca174aad82c8634d132bf0c49febef6f
Aseel-Z <aseelalzweri@gmail.com>
2021-05-05 14:34:29 +0300

    v1-2

--- madlib_cli/madlib_cli.py
+++ madlib_cli/madlib_cli.py
@@ -1,57 +1,83 @@
+import errno
+import os
+import re
+
 def intro():
     welcomeMsg= """Welcome to MadLib Command Line (MADLIB-CLI)!
     Ready for some fun?  Guess the words! :)
     """
     print(welcomeMsg)
 
-intro()
+intro
 
-def read_template():
-    template = open('assets/a_spring_day_template.txt','r')
-    contents = template.read()
-    line_contents=template.readlines()
-    template.close()
-    print(contents)
-    print(line_contents)
 
-    for line in line_contents:
+#Create and test a read_template function that takes in a path to text file and returns a stripped string of the file’s contents
+# read_template should raise a FileNotFoundError if path is invalid.
+
+def read_template(path):
+    if os.path.isfile():
+        template = open(path,'r')
+        contents = template.read()
+        stripped = contents.strip()
+        template.close()
+        return stripped
+    else:
+        raise FileNotFoundError()
+
+
+
+
+#Create and test a parse_template function that takes in a template string and 
+# returns a string with language parts removed and a separate list of those language parts.
+
+def parse_template(template):
+    empty_string = re.sub(r"\s*{.*}\s*", template)
+    language_parts = re.findall(r"\s*{.*}\s*", " ", template)
+    return string, language_parts
+
 
 
 
 def getUserInputs():
-    	
-Adjective = input("How would you describe a spring day?: ")
-Noun1 = input("Enter an adjective: ")
-Place1 = input("Enter an adjective: ")
-Noun2 = input("Enter an adjective: ")
-Name = input("Enter an adjective: ")
-Place2 = input("Enter an adjective: ")
-Thing = input("Enter an adjective: ")
-Activity = input("Enter an adjective: ")
+    Adjective = input("How would you describe a spring day? ")
+    Noun1 = input("Enter something that is located up the land?")
+    Place1 = input("A place where poeple eat and meet? ")
+    Noun2 = input("a wide blue thing?")
+    Name = input("a person's name? a female maybe?")
+    Place2 = input("a place where people buy stuff? ")
+    Thing = input("Something we can't suvive without? ")
+    Activity = input(" the act of buying? ")
+    return {Adjective, Noun1, Place1, Noun2, Name, Place2, Thing, Activity}
 
-Today, was an {Adjective} spring day. The {Noun1} was blue and clear.
-I went to a {Place1} near the {Noun2} with {Name}.
-Then, I went to the {Place2} to do my {Thing} {Activity}.
+#  Create and test a merge function that takes in a “bare” template and a list of user entered language parts, 
+# and returns a string with the language parts inserted into the template
+# def merge(empty_template, inputs):
+#     for i in inputs:
+#         empty_template.format(,inputs[i])
+#     return empty_template
 
 
- 
+
+# def read_template():
+#     template = open('assets/a_spring_day_template.txt','r')
+#     contents = template.read()
+#     line_contents=template.readlines()
+#     for line in 
+#     template.close()
+
+#     # print(contents)
+#     # print(line_contents)
+#     inputs = getUserInputs()
+#     print(inputs)
+#     for element in inputs:
+#         print(element)
+#         contents = contents.replace("{element}", element)
+#     print(contents)
+#     template.close()
+
+
 
  
-# # # Ask the user to input a noun
-# # noun = input("Enter a noun: ")
- 
-# # # Replace the blank with the user's input
-# # madlib = madlib.replace("blank", noun)
- 
-# # # Print out the Mad Lib including the user's response
-# # print(madlib)
-
-	
-# ​string = "This is a string"
- 
-# string = string.replace("is", "was")
- 
-# print(string)
 
 
 
--- tests/test_madlib_cli.py
+++ tests/test_madlib_cli.py
@@ -3,3 +3,22 @@ from madlib_cli import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_read_template():
+    actual = read_template('assets/a_spring_day_template.txt')
+    expected = '''
+    Today, was an awesome spring day. The sky was blue and clear.
+    I went to a cafe near the sea with Sarah.
+    Then, I went to the supermarket to do my grocery shopping.
+    '''
+    assert actual==expected
+
+def test_parse_template():
+    actual = parse_template('It was a {Adjective} and {Adjective} {Noun}.')
+    expected = "It was a   and    ."
+    assert actual==expected 
+
+def test_merge():
+    actual = merge('It was a {} and {} {}.', ['rainy', 'windy', 'day'])
+    expected = "It was a raing and stormy day."
+    assert actual==expected 
